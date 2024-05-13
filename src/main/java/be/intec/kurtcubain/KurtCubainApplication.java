763576d66de0ddbb838dca6e2eb7e96fa8b8de45
raphaelzolotarev <raphaelzolotarev@gmail.com>
2024-05-13 13:26:44 +0200

    start

---
+++ src/main/java/be/intec/kurtcubain/KurtCubainApplication.java
@@ -0,0 +1,13 @@
+package be.intec.kurtcubain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurtCubainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurtCubainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/intec/kurtcubain/KurtCubainApplicationTests.java
@@ -0,0 +1,13 @@
+package be.intec.kurtcubain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurtCubainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
